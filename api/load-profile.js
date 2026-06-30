48afec1671ce98cd9d2dfc919c0bc0ec70345e6f
rabinshmulik-a11y <rabinshmulik@gmail.com>
2026-06-30 10:29:13 -0400

    Create load-profile.js

---
+++ api/load-profile.js
@@ -0,0 +1,18 @@
+import { supabaseFetch } from './supabase.js';
+
+export default async function handler(req, res) {
+  try {
+    const response = await supabaseFetch(
+      'users?select=*&id=eq.3'
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json(data[0] || null);
+
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
+    });
+  }
+}
